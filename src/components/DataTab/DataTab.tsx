395a751b7c2326c1c2645fc9040375dabc829f43
Anterul <andre.shale@yandex.ru>
2024-03-28 16:32:04 +0300

    feat: сверстать и начать продувыть логику для нижней панели пагинации

--- src/components/DataTab/DataTab.tsx
+++ src/components/DataTab/DataTab.tsx
@@ -9,10 +9,22 @@ import PaginationBar from "../PaginationBar/PaginationBar";
 function DataTab() {
   // стейт для массива с персонажами
   const [customers, setCharacters] = useState([]);
+
   // стейт для массива с локациями
   const [locations, setLocations] = useState([]);
 
   // const result: any[] = [];
+
+  // счетчик постов с api
+  const [apiInfoCount, setApiInfoCount] = useState(0);
+
+  // счетчик общего количества страниц с учетом rows per page(округление в +)
+  function countPagesNumber(): number {
+    return Math.ceil(apiInfoCount / elementsToDisplay);
+  }
+
+  //
+  const [elementsToDisplay, setElementsToDisplay] = useState(15);
 
   function getCharacters(): any {
     Api.getCharacters();
@@ -29,7 +41,9 @@ function DataTab() {
       .then((response: any) => {
         if (!response) return;
         setCharacters(response.data.results);
+        setApiInfoCount(response.data.info.count);
         console.log(customers);
+        console.log(response.data.info.count);
       })
       .catch((error: any) => {
         console.error("Error fetching characters:", error);
@@ -44,13 +58,16 @@ function DataTab() {
     */
   }, []);
 
+  // кнопки для разработки
   function customersState(): any {
     console.log(customers);
     console.log(customers[0]["name"]);
+    console.log(apiInfoCount);
+    console.log(countPagesNumber());
   }
 
   // рендер строк
-  const elementsToDisplay: number = 15;
+  // количество строк, которое нужно разместить на одной странице
   const slicedElements = customers.slice(0, elementsToDisplay);
   const renderdElements = slicedElements.map((item) => (
     <Customer customer={item} key={item["id"]} />
@@ -76,7 +93,12 @@ function DataTab() {
         customers
       </button>
       <ul className="data-tab__ul">{renderdElements}</ul>
-      <PaginationBar />
+      <PaginationBar
+        rowsPerPageCounter={elementsToDisplay}
+        pagesNumberCounter={countPagesNumber()}
+        elementsToDisplay={elementsToDisplay}
+        setElementsToDisplay={setElementsToDisplay}
+      />
     </div>
   );
 }
--- src/components/PaginationBar/PaginationBar.tsx
+++ src/components/PaginationBar/PaginationBar.tsx
@@ -1,9 +1,76 @@
-import "./PaginationBar.scss";
+import { useState } from "react";
 
-function PaginationBar() {
+import "./PaginationBar.scss";
+import pageSelectorButton from "../../images/pageSelectorButton.svg";
+
+function PaginationBar(props: any) {
+  function pagesNumberCounter(): number {
+    return props.pagesNumberCounter;
+  }
+
+  function handleSelectChange(event: any): any {
+    props.setElementsToDisplay(event.target.value);
+  }
+
+  const [isSelectorOpen, setIsSelectorOpen] = useState(false);
+  function handeleSelectorOpen() {
+    setIsSelectorOpen(!isSelectorOpen);
+  }
+
   return (
     <div className="pagination-bar">
+      <p className="pagination-bar__total-rows">
+        1-2 of {pagesNumberCounter()}
+      </p>
       <p className="pagination-bar__rows-note">Rows per page:</p>
+
+      <p className="pagination-bar__rows-per-page-counter">
+        {props.rowsPerPageCounter}
+      </p>
+
+      <select
+        className={`pagination-bar__rows-per-page-selector ${
+          isSelectorOpen ? "pagination-bar_open" : ""
+        }`}
+        size={4}
+        value={props.elementsToDisplay}
+        onChange={handleSelectChange}
+      >
+        <option className="pagination-bar__selector-option" value={5}>
+          5
+        </option>
+        <option className="pagination-bar__selector-option" value={10}>
+          10
+        </option>
+        <option className="pagination-bar__selector-option" value={15}>
+          15
+        </option>
+        <option className="pagination-bar__selector-option" value={20}>
+          20
+        </option>
+      </select>
+
+      <button
+        className="pagination-bar__page-selector"
+        type="button"
+        onClick={handeleSelectorOpen}
+      >
+        <img src={pageSelectorButton} alt="selector button" />
+      </button>
+
+      <button
+        className="pagination-bar__moving-button pagination-bar__moving-button_direction_back"
+        type="button"
+      ></button>
+
+      <p className="agination-bar__current-page-counter">
+        1 / {pagesNumberCounter()}
+      </p>
+
+      <button
+        className="pagination-bar__moving-button pagination-bar__moving-button_direction_forward"
+        type="button"
+      ></button>
     </div>
   );
 }
